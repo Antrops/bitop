ffd5a58f531eb3a4fc91ffe04e19077f5bd3b48b
Antrops <30654111+Antrops@users.noreply.github.com>
2017-08-04 14:45:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
 
+bitmod = Extension('bitop',
+                    sources = ['bitop.c'])
+
 setup(
   name = 'bitop',
   packages = ['bitop'],
   version = '0.5',
   description = 'A simple bitwise operator handling module.',
+  ext_modules = [bitmod],
   author = 'Antrops',
   author_email = 'officialantrops@gmail.com',
   url = 'https://github.com/Antrops/bitop', 
