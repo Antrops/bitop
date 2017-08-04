50cd415ffb2fe1458325404fb887de5849ada982
Antrops <30654111+Antrops@users.noreply.github.com>
2017-08-04 14:26:48 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name = 'bitop',
+  packages = ['bitop'],
+  version = '0.5',
+  description = 'A simple bitwise operator handling module.',
+  author = 'Antrops',
+  author_email = 'officialantrops@gmail.com',
+  url = 'https://github.com/Antrops/bitop', 
+  keywords = ['bits', 'operators', 'bitwise', 'python', 'logic', 'binary'],
+  classifiers = [],
+)
